32563e8f22ea165152e7bb42a040f30c9b63a33e
Svetlomir Delsizov <freeway88@abv.bg>
2024-12-19 01:03:47 +0200

    backend
    the other versions of Back End deleted and only one left.

--- Soft Uni Academy/06.JS Angular/Project-Exam/backend/src/router/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authController } = require('../controllers/authController');
-const { auth } = require('../utils/auth');
-
-router.get('/profile', auth(),authController.getProfileInfo);
-router.put('/profile', auth(),authController.editProfileInfo);
-
-module.exports = router

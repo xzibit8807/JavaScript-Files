4637f65db60e78d584d3883d26fb67030f5ad58b
Svetlomir Delsizov <freeway88@abv.bg>
2025-03-03 01:54:21 +0200

    fixed post Date for this project

--- Soft Uni Academy/07. React/FirstTestWorkshop/client/src/services/userService.js
+++ Soft Uni Academy/07. React/FirstTestWorkshop/client/src/services/userService.js
@@ -9,12 +9,19 @@ export default {
     },
     // getAllUsers: () => fetch(url).then(res => res.json())
     async create(userData){
+        const { country, city, street, streetNumber, postData} = userData;
+
+        postData.address = {country, city, street, streetNumber};
+        postData.createdAt = new Date().toISOString();
+        postData.updatedAt = new Date().toISOString();
+
+
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(userData)
+            body: JSON.stringify(postData)
         });
         return await response.json(); 
     }

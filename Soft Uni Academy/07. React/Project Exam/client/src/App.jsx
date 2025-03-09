4469459dae08f0f7100e7466cdfe148e95f46d94
Svetlomir Delsizov <freeway88@abv.bg>
2025-03-09 16:47:09 +0200

    Starting with the Game World Catalog
    Seting aditional parts of the HTML to separet modules.
    Afthers many trys I decided to be a Game Catalog Page.

---
+++ Soft Uni Academy/07. React/Project Exam/client/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import FOoterComp from "./modules/Footer";
+import HeaderComponent from "./modules/Header";
+import MainComp from "./modules/Main";
+
+
+function App() {
+  return (
+    <>
+  <HeaderComponent/>
+  <MainComp/>
+  <FOoterComp/> 
+    </>
+  );
+}
+
+export default App;
